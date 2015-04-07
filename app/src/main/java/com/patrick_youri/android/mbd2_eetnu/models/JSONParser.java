3641d8baec8db59a497a841478689ff024c86c21
YourivanHeumen <youri.v.h.13@hotmail.com>
2015-04-07 21:57:54 +0200

    class met 2 methodes gemaakt, met getJSON(url) kan de json opgehaald worden van de api en met parseJSONResponse(json) worden er objecten gemaakt van de json data en worden deze als arraylist gereturnt

---
+++ app/src/main/java/com/patrick_youri/android/mbd2_eetnu/models/JSONParser.java
@@ -0,0 +1,77 @@
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Youri on 7-4-2015.
+ */
+public abstract class JSONParser {
+
+    public static String getJSON(URL url) throws IOException {
+        StringBuilder response = new StringBuilder();
+
+        HttpURLConnection httpconn = (HttpURLConnection)url.openConnection();
+
+        if (httpconn.getResponseCode() == HttpURLConnection.HTTP_OK)
+        {
+            BufferedReader input = new BufferedReader(new InputStreamReader(httpconn.getInputStream()),8192);
+            String strLine = null;
+            while((strLine = input.readLine()) != null)
+            {
+                response.append(strLine);
+            }
+            input.close();
+        }
+        return response.toString();
+    }
+
+    public static List<Venue> parseJSONResponse(String json){
+        List<Venue> venues = new ArrayList<Venue>();
+
+        try {
+            JSONArray jsonArray = new JSONObject(json).getJSONArray("results");
+
+            for(int i = 0; i < jsonArray.length(); i++)
+            {
+                JSONObject jsonVenueObject = jsonArray.getJSONObject(i);
+
+                int id = jsonVenueObject.getInt("id");
+                String name = jsonVenueObject.getString("name");
+                String category = jsonVenueObject.getString("category");
+                String website_url = jsonVenueObject.getString("website_url");
+                String telephone = jsonVenueObject.getString("telephone");
+                double distance = jsonVenueObject.getDouble("distance");
+
+                JSONObject jsonAddressObject = jsonVenueObject.getJSONObject("address");
+                String street = jsonAddressObject.getString("street");
+                String zipcode = jsonAddressObject.getString("zipcode");
+                String city = jsonAddressObject.getString("city");
+                String region = jsonAddressObject.getString("region");
+                String country = jsonAddressObject.getString("country");
+
+                JSONObject jsonGeolocationObject = jsonVenueObject.getJSONObject("geolocation");
+                double latitude = jsonGeolocationObject.getDouble("latitude");
+                double longitude = jsonGeolocationObject.getDouble("longitude");
+
+                Venue venue = new Venue(id, name, category, website_url, telephone,
+                                        new Address(street, zipcode, city, region, country),
+                                        new Geolocation(latitude, longitude), distance);
+
+                venues.add(venue);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return venues;
+    }
+}
