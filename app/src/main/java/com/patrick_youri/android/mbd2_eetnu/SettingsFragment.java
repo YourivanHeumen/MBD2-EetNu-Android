bb2f40c0a30b07e848a1ec49f66ff7d797dad7cf
PatrickVanOverdijk <p.vanoverdijk@student.avans.nl>
2015-04-09 21:58:32 +0200

    preferences

--- app/src/main/java/com/patrick_youri/android/mbd2_eetnu/SettingsFragment.java
+++ app/src/main/java/com/patrick_youri/android/mbd2_eetnu/SettingsFragment.java
@@ -2,11 +2,17 @@ package com.patrick_youri.android.mbd2_eetnu;
 
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 public class SettingsFragment extends PreferenceFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        PreferenceManager.getDefaultSharedPreferences();
+
+        //link met waar in onderanderen heb geprobeert te zoeken hoe deze shit werkt
+        // http://www.cs.dartmouth.edu/~campbell/cs65/lecture12/lecture12.html
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.settings_preference);
