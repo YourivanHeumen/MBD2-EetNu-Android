fb001ac16eda0fcd91c415bcdc2889bf548f3b92
YourivanHeumen <youri.v.h.13@hotmail.com>
2015-04-10 16:02:22 +0200

    Call werkt

--- app/src/main/java/com/patrick_youri/android/mbd2_eetnu/DetailFragment.java
+++ app/src/main/java/com/patrick_youri/android/mbd2_eetnu/DetailFragment.java
@@ -1,11 +1,17 @@
 package com.patrick_youri.android.mbd2_eetnu;
 
 import android.app.Fragment;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.patrick_youri.android.mbd2_eetnu.models.Venue;
 
@@ -31,5 +37,32 @@ public class DetailFragment extends Fragment {
         detailsStreet.setText(venue.getAddress().getStreet());
         detailsPostalcode.setText(venue.getAddress().getZipcode());
         detailsPhone.setText(venue.getTelephone());
+
+        Button callButton = (Button)getView().findViewById(R.id.callbutton);
+        callButton.setOnClickListener(new CallButtonOnClickListener(venue));
     }
+
+    public class CallButtonOnClickListener implements View.OnClickListener {
+        private Venue venue;
+
+        public CallButtonOnClickListener(Venue venue) {
+            this.venue = venue;
+        }
+
+        @Override public void onClick(View v) {
+            if (venue.getTelephone() != null) {
+                try {
+                    Intent callIntent = new Intent(Intent.ACTION_CALL);
+                    callIntent.setData(Uri.parse("tel:" + venue.getTelephone()));
+                    startActivity(callIntent);
+                } catch (ActivityNotFoundException activityException) {
+                    Toast.makeText(getActivity(), "Call failed", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(getActivity(), "Call failed, venue have no phonenumber", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
 }
